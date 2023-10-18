08d2705607ddd88ab5030bab6c5b2d8443830425
Eemen <egemenkeskin61@gmail.com>
2023-10-18 08:49:08 +0200

    palindrom untersucher fertig
    palindrom untersucher fertig

--- src/at/eee/basic/Stringhelper.java
+++ src/at/eee/basic/Stringhelper.java
@@ -3,15 +3,34 @@ package at.eee.basic;
 import java.util.Scanner;
 
 public class Stringhelper {
-    static String textstring = "hallo";
+    static String textstring = "hallah";
 
     public static void main(String[] args) {
         char[] textarray = toArray(textstring);
-        System.out.println(textarray[2]);
+        if (isPalindrome(textarray)) {
+            System.out.println("Das Wort ist ein Palindrom.");
+        } else {
+            System.out.println("Das Wort ist kein Palindrom.");
+        }
     }
 
     public static char[] toArray(String textstring) {
         char[] textarray = textstring.toCharArray();
         return textarray;
+    }
+
+    public static boolean isPalindrome(char[] textarray) {
+        int start = 0;
+        int end = textarray.length - 1;
+
+        while (start < end) {
+            if (textarray[start] != textarray[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
