3ecb64f07f22e7ace0f0494344d27a010b1b6864
Eemen <egemenkeskin61@gmail.com>
2023-10-20 08:01:57 +0200

    tiktaktoe noch nicht fertig
    tiktaktoe noch nicht fertig

--- src/at/eee/basic/TikTakToe.java
+++ src/at/eee/basic/TikTakToe.java
@@ -1,19 +1,25 @@
 package at.eee.basic;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class TikTakToe {
-    int[] verfuegbar = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    static int[] verfuegbar = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static void main(String[] args) {
-        begruesung();
-        durchlauf();
+       //begruesung();
+       //durchlauf();
+       spielfeld(verfuegbar);
     }
 
-    public static void begruesung(){
+    /*public static void begruesung(){
         System.out.println("Willkommen zum TikTakToe spiel.");
         System.out.println("Das ist das Spielfeld mit den Nummern der Feldern");
         System.out.println("|1|2|3|\n|4|5|6|\n|7|8|9|\n");
+
+
+
+
     }
 
     public static void durchlauf(){
@@ -22,13 +28,22 @@ public class TikTakToe {
         Scanner scanner = new Scanner(System.in);
         int selection = scanner.nextInt();
 
-        System.out.println("Wo möchte Spieler 1 sein Feld plazieren.");
+        System.out.println("An welche Feld möchte Spieler 1 sein Feld plazieren.");
+        System.out.println("|1|2|3|\n|4|5|6|\n|7|8|9|");
         int einzahlungsBetrag = scanner.nextInt();
         spielfeld(einzahlungsBetrag);
-        System.out.println("|1|2|3|\n|4|5|6|\n|7|8|9|");
-    }
-    public static void spielfeld(int a){
-        
+
+    }*/
+    public static void spielfeld(int[] array){
+
+        System.out.println(array[0]);
+
+        for (int i = 0; i < array.length; i++) {
+
+            if(i<9)
+            System.out.println(array[i+1]);
+
+        }
 
     }
 }
@@ -37,4 +52,4 @@ for (int i = 0; i < verfuegbar.length; i++) {
         if (verfuegbar[i] == gesuchterWert) {
         gefunden = true;
         break; // Wenn der Wert gefunden wurde, breche die Schleife ab.
-        }
+        }*/
