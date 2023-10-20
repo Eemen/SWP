fc6ebcef9c7fa4e5459d761c4a5bb3a56a91a3b2
Eemen <egemenkeskin61@gmail.com>
2023-10-20 09:26:41 +0200

    TikTakToe noch nicht fertig
    TikTakToe noch nicht fertig

--- src/at/eee/basic/TikTakToe.java
+++ src/at/eee/basic/TikTakToe.java
@@ -5,46 +5,72 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TikTakToe {
-    static int[] verfuegbar = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    static char[] verfuegbar = {'1', '2', '3', '4', '5', '6', '7','8', '9'};
+
     public static void main(String[] args) {
-       //begruesung();
-       //durchlauf();
-       spielfeld(verfuegbar);
+       begruesung();
+       durchlauf();
     }
 
-    /*public static void begruesung(){
+    public static void begruesung(){
         System.out.println("Willkommen zum TikTakToe spiel.");
         System.out.println("Das ist das Spielfeld mit den Nummern der Feldern");
         System.out.println("|1|2|3|\n|4|5|6|\n|7|8|9|\n");
-
-
-
-
     }
 
     public static void durchlauf(){
         boolean platzvergeben = false;
-
+        boolean spieler;
         Scanner scanner = new Scanner(System.in);
         int selection = scanner.nextInt();
 
         System.out.println("An welche Feld möchte Spieler 1 sein Feld plazieren.");
         System.out.println("|1|2|3|\n|4|5|6|\n|7|8|9|");
         int einzahlungsBetrag = scanner.nextInt();
-        spielfeld(einzahlungsBetrag);
+        spieler = true;
+        spielfeld(selection,  verfuegbar, spieler);
 
-    }*/
-    public static void spielfeld(int[] array){
-
-        System.out.println(array[0]);
-
-        for (int i = 0; i < array.length; i++) {
-
-            if(i<9)
-            System.out.println(array[i+1]);
-
+    }
+    public static void spielfeld(int a, char[] array, boolean spielr){
+        char welcherspieler;
+        if (spielr) {
+            welcherspieler = 'X';
+        }
+        else {
+            welcherspieler = 'Y';
+        }
+        switch(a) {
+            case 1:
+                array[0] = welcherspieler;
+                break;
+            case 2:
+                array[1] = welcherspieler;
+                break;
+            case 3:
+                array[2] = welcherspieler;
+                break;
+            case 4:
+                array[3] = welcherspieler;
+                break;
+            case 5:
+                array[4] = welcherspieler;
+                break;
+            case 6:
+                array[5] = welcherspieler;
+                break;
+            case 7:
+                array[6] = welcherspieler;
+                break;
+            case 8:
+                array[7] = welcherspieler;
+                break;
+            case 9:
+                array[8] = welcherspieler;
+                break;
+            default:// code block
         }
 
+        System.out.println("|"+ array[0] + "|" + array[1] + "|" + array[2] + "|" + "\n" + "|" + array[3] +"|" + array[4] + "|" + array[5] + "|" + "\n" + "|" + array[6] + "|" + array[7] + "|" + array[8] + "|" + "\n");
     }
 }
 /*
