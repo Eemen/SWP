65580dda46ba9f8b681a4282d0c133b49a5d3eb0
Eemen <egemenkeskin61@gmail.com>
2023-11-14 14:51:02 +0100

    noch nicht fertig

---
+++ src/at/eee/examples/Figuren/Figure.java
@@ -0,0 +1,11 @@
+package at.eee.examples.Figuren;
+
+public class Figure {
+    String Name;
+    String type;
+    int a;
+    int b;
+    int r;
+
+
+}
---
+++ src/at/eee/examples/Figuren/TestIT.java
@@ -0,0 +1,7 @@
+package at.eee.examples.Figuren;
+
+public class TestIT {
+    public static void main(String[] args) {
+
+    }
+}
