0065b53c56946becb007e634e2154f71cad574d5
Eemen <egemenkeskin61@gmail.com>
2023-11-14 15:49:36 +0100

    Figuren
    fast fertig

--- src/at/eee/examples/Figuren/Figure.java
+++ src/at/eee/examples/Figuren/Figure.java
@@ -5,7 +5,79 @@ public class Figure {
     String type;
     int a;
     int b;
-    int r;
+    double r;
 
+    public Figure(String name, String type, int a, int b, double r) {
+        Name = name;
+        this.type = type;
+        this.a = a;
+        this.b = b;
+        this.r = r;
+    }
 
+    //Fuktion
+    public void getArea(String a){
+        switch (a){
+            case "kreis":
+                double x = this.getR();
+                double y = x * x * 3.14159;
+                System.out.println("Der Flächeninhalt des Kreises ist: " + y);
+                break;
+            case "viereck":
+                int b = this.getA();
+                int c = this.getB();
+                int d = b * c;
+                System.out.println("Der Flächeninhalt des Vierecks ist: " + d);
+                break;
+            case "quadrat":
+                b = this.getA();
+                c = b * b;
+                System.out.println("Der Flächeninhalt des Quadrats ist: " + c);
+                break;
+            case "würfel":
+                b = this.getA();
+                c = (b * b)
+        }
+    }
+
+    //getter and setter
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
 }
--- src/at/eee/examples/Figuren/TestIT.java
+++ src/at/eee/examples/Figuren/TestIT.java
@@ -2,6 +2,13 @@ package at.eee.examples.Figuren;
 
 public class TestIT {
     public static void main(String[] args) {
+        Figure k1 = new Figure("Kreis1", "kreis", 0 ,0 ,10);
+        Figure v1 = new Figure("Viereck1", "viereck",2, 3, 0);
+        Figure q1 = new Figure("Quadrat1", "quadrat",4,0 , 0);
+        Figure w1 = new Figure("Würfel", "würfel", 6, 0, 0);
 
+        k1.getArea(k1.getType());
+        v1.getArea(v1.getType());
+        q1.getArea(q1.getType());
     }
 }
